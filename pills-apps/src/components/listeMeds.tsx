bdc7245b8ddda270a58996b8e567ff9347ef9795
Juliette <juliette.mathivet@gmail.com>
2025-03-08 22:14:07 -0500

    requete get implemented

--- pills-apps/src/components/listeMeds.tsx
+++ pills-apps/src/components/listeMeds.tsx
@@ -1,17 +1,46 @@
 import Accordion from 'react-bootstrap/Accordion';
 import {User} from '../classes/User.js' 
 import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import './listemeds.css'
-import mockUser from '../mockUser.json';
+import { Medicine } from '../classes/Medecine.js';
+
 
 function BasicExample() {
   const location = useLocation();
-  //const {user}: {user: User} = location.state || {};
+  const {user}: {user: User} = location.state || {};
+  const [medications, setMedications] = useState<Medicine[]>([]);
+  
+  useEffect(() => {
+    const fetchMedications = async () => {
+      try {
+        // Effectuer les requêtes pour récupérer les détails des médicaments
+        const meds = await Promise.all(
+          user.prescriptionList.map(async (medId) => {
+            const response = await fetch(`http://localhost:3000/api/medicines/${medId}`); // Remplacer par l'URL de ton API
+            if (!response.ok) {
+              throw new Error('Erreur lors de la récupération des données');
+            }
+            const data = await response.json(); // Transformer la réponse en JSON
+            return data; // Retourner les données du médicament
+          })
+        );
+        setMedications(meds); // Mettre à jour l'état avec les médicaments récupérés
+      } catch (error) {
+        console.error('Erreur lors de la récupération des médicaments:', error);
+      }
+    };
+
+    if (user && user.prescriptionList.length > 0) {
+      fetchMedications(); // Appeler la fonction de récupération des médicaments
+    }
+  }, [user]); // Réexécuter si `us
+  
     return (
       <>
-        <h1 className='title'>Bonjour, {mockUser.name}</h1>
+        <h1 className='title'>Bonjour, {user.name}</h1>
         <Accordion defaultActiveKey="0">
-          {mockUser.prescriptionList.map((med, index) => ( // changer liste par celle qui aura été  générée par Félix.
+          {medications.map((med, index) => ( // changer liste par celle qui aura été  générée par Félix.
             <Accordion.Item eventKey={String(index)} key={index}>
               <Accordion.Header>{med.medicineName}</Accordion.Header>
               <Accordion.Body>
